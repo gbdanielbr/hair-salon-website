3737587dcb1495f89d63dc81a0e7a671c354cefc
Daniel M <daniel.milli@hotmail.com>
2021-11-03 15:17:21 -0300

    Added image on section about and script to close the menu list when clicking the item

--- main.js
+++ main.js
@@ -1,2 +1,18 @@
 const nav = document.querySelector('#header nav')
 const toggle = document.querySelectorAll('nav .toggle')
+// MENU ABRE E FECHA QUANDO CLICA NOS ICONES
+
+for(const element of toggle) {
+  element.addEventListener('click', () => {
+    nav.classList.toggle('show')
+  })
+}
+
+// MENU FECHA QUANDO CLICA NO ITEM DA LISTA DE MENU
+const links = document.querySelectorAll('nav ul li a')
+
+for (const link of links) {
+  link.addEventListener('click', () => {
+    nav.classList.remove('show')
+  })
+}
