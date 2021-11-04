4dba9370fc4cddc05502454fb16059cc5134da59
Daniel M <daniel.milli@hotmail.com>
2021-11-04 12:09:23 -0300

    Home, Menu, About and Services sections: Script and Styles updated

--- main.js
+++ main.js
@@ -16,3 +16,15 @@ for (const link of links) {
     nav.classList.remove('show')
   })
 }
+
+// MUDAR O HEADER QUANDO SCROLL
+const header = document.querySelector("#header")
+const navHeight = header.offsetHeight
+
+window.addEventListener('scroll', () => {
+  if(window.scrollY >= navHeight) {
+    header.classList.add('scroll')
+  } else {
+    header.classList.remove('scroll')
+  }
+})
