fd8fb8e16f81833c788fb3777553b1adbb31a359
Daniel M <daniel.milli@hotmail.com>
2021-11-05 10:44:46 -0300

    Added carousel styles for testimonials container

--- main.js
+++ main.js
@@ -28,3 +28,13 @@ window.addEventListener('scroll', () => {
     header.classList.remove('scroll')
   }
 })
+
+// SWIPER
+const swiper = new Swiper('.swiper', {
+  slidesPerView: 1,
+  pagination: {
+    el: '.swiper-pagination'
+  },
+  mousewheel: true,
+  keyboard: true,
+});
